ccddbad9c291f367b789d2fdff8df503fb776bb3
Vaishnav <144126360+PintuVaishnav@users.noreply.github.com>
2025-09-21 16:56:03 +0530

    Update teacherSlice.js

--- frontend/src/redux/teacherRelated/teacherSlice.js
+++ frontend/src/redux/teacherRelated/teacherSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-const REACT_APP_BASE_URL = "http://localhost:5000";
+const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const initialState = {
     teachersList: [],
